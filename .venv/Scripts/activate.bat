360aee59b3bd45cc7420a1a53c3a79402f128861
maxmasprim <michaelmwasame6@gmail.com>
2026-08-11 23:04:50 +0300

    updated requirements

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\PJ\bidii-credit-backend\.venv
+set VIRTUAL_ENV=C:\Users\User\Desktop\PJ\Bidii Credit Webiste Pj\bidii-credit-backend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
